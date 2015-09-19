efea51a1a91da31a769dc330efc5d4a2b40cf957
Greg Brown <greg@12dnetworks.com>
2015-09-19 11:58:32 -0700

    re #6 put wbkrux into requirejs as well

--- view/shared/assets/js/wb/ads/latest/krux.js
+++ view/shared/assets/js/wb/ads/latest/krux.js
@@ -7,6 +7,12 @@
  *
  * This script must be included AFTER wbads is included.
  *
+ * Note that although this looks like a nice and tidy UMD implementation, it's not, it's
+ * adding the module to all loaders if found AND the window global.  This is because
+ * we have mixed use of globals, requirejs, etc.  For now, this is the least putrid
+ * setup so sites using full requirejs don't have to define wbkrux separately.
+ * wbppid (ppid.js) is also doing this.
+ *
  * @todo: consider rewriting all wbads and plugins as amd modules or at least with umd
  *
  * @requires window.KRUX_CONFID
@@ -16,41 +22,34 @@
 /*global Krux: true, wbads: true */
 
 window.KRUX_CONFID = window.KRUX_CONFID || false;
+window.KRUX_VERSION = window.KRUX_VERSION || '1.9';
 window.Krux||((Krux=function(){Krux.q.push(arguments)}).q=[]);
 
-(function(w, d, wbads, krux) {
+(function(w, d, wbads, krux, factory) {
+  'use strict';
+  w.wbkrux = factory(w, d, wbads, krux);
+
+  // Montage Require
+  if (typeof bootstrap === 'function') {
+    bootstrap('wbkrux', w.wbkrux);
+  }
+
+  // CommonJS/Node
+  if (typeof exports === 'object') {
+    module.exports = w.wbkrux;
+  }
+
+  // RequireJS
+  if (typeof define === 'function' && define.amd) {
+    define('wbkrux', [], w.wbkrux);
+  }
+
+})(window, document, window.wbads, window.Krux, function(w, d, wbads, krux) {
   'use strict';
 
-  var ctagId = 'kxct',
-    ctag = d.getElementById(ctagId);
-
-  if (ctag && !w.KRUX_CONFID) {
-    w.KRUX_CONFID = ctag.getAttribute('data-id') || false;
-  }
-
-  if (!w.KRUX_CONFID) {
-    return;
-  }
-
-  if (!ctag) {
-    ctag = d.createElement('script');
-    ctag.type = 'text/javascript';
-    ctag.id = ctagId;
-    ctag.setAttribute('class', ctagId);
-    ctag.setAttribute('data-id', w.KRUX_CONFID);
-    ctag.setAttribute('data-timing', 'async');
-    ctag.setAttribute('data-version', '1.9');
-    var s = d.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(ctag, s);
-
-    var k = d.createElement('script');
-    k.type = 'text/javascript';
-    k.async = true;
-    var m, src = (m=location.href.match(/\bkxsrc=([^&]+)/)) && decodeURIComponent(m[1]);
-    k.src = /^https?:\/\/([a-z0-9_\-\.]+\.)?krxd\.net(:\d{1,5})?\//i.test(src) ? src : src === 'disable' ? '' :
-        (location.protocol === 'https:' ? 'https:' : 'http:') + '//cdn.krxd.net/controltag?confid=' + w.KRUX_CONFID;
-    s.parentNode.insertBefore(k, s);
-  }
+  var _this = {};
+  var ctagId = 'kxct';
+  var ctag = d.getElementById(ctagId);
 
   /**
    * Helper function to retrieve krux data from localstorage or cookies.
@@ -111,6 +110,40 @@ window.Krux||((Krux=function(){Krux.q.push(arguments)}).q=[]);
     return encode ? encodeURIComponent(str) : str;
   }
 
+  if (ctag && !w.KRUX_CONFID) {
+    w.KRUX_CONFID = ctag.getAttribute('data-id') || false;
+  }
+
+  // public interface
+  _this.getParam = getParam;
+  _this.getUser = getUser;
+  _this.getSegments = getSegments;
+  _this.getGptCustParams = getGptCustParams;
+
+  if (!w.KRUX_CONFID) {
+    return _this;
+  }
+
+  if (!ctag) {
+    ctag = d.createElement('script');
+    ctag.type = 'text/javascript';
+    ctag.id = ctagId;
+    ctag.setAttribute('class', ctagId);
+    ctag.setAttribute('data-id', w.KRUX_CONFID);
+    ctag.setAttribute('data-timing', 'async');
+    ctag.setAttribute('data-version', w.KRUX_VERSION);
+    var s = d.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ctag, s);
+
+    var k = d.createElement('script');
+    k.type = 'text/javascript';
+    k.async = true;
+    var m, src = (m=location.href.match(/\bkxsrc=([^&]+)/)) && decodeURIComponent(m[1]);
+    k.src = /^https?:\/\/([a-z0-9_\-\.]+\.)?krxd\.net(:\d{1,5})?\//i.test(src) ? src : src === 'disable' ? '' :
+        (location.protocol === 'https:' ? 'https:' : 'http:') + '//cdn.krxd.net/controltag?confid=' + w.KRUX_CONFID;
+    s.parentNode.insertBefore(k, s);
+  }
+
   wbads.defineCallback('pre.enable.services', function() {
     krux.user = getUser();
     krux.segments = getSegments();
@@ -120,15 +153,5 @@ window.Krux||((Krux=function(){Krux.q.push(arguments)}).q=[]);
     wbads.setGlobalTargetingParam('khost', encodeURIComponent(d.location.hostname));
   });
 
-  /**
-   * Attach helper functions to window so we can get krux data
-   * for other modules (like kwidget for customParams).
-   */
-  w.wbkrux = {
-    getParam: getParam,
-    getUser: getUser,
-    getSegments: getSegments,
-    getGptCustParams: getGptCustParams
-  };
-
-}(window, document, window.wbads, window.Krux));
+  return _this;
+});
